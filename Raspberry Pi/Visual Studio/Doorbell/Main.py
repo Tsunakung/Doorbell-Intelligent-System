a823030dc5071f5134ba50c3e95c4c5591ae570c
LewTsu <lewlaao@hotmail.com>
2016-01-12 16:13:03 +0700

    Revert "Revert "Update Visual Studio Python Code""
    This reverts commit 101047fe0ff80528aa4497b2e8b4185e1374b15a.

---
+++ Raspberry Pi/Visual Studio/Doorbell/Main.py
@@ -0,0 +1,12 @@
+﻿import SocketServer
+import StreamServer
+
+startSocket = SocketServer()
+startSocket.start()
+
+startStream = StreamServer()
+startStream.start()
+
+#ไม่ต้องมีเวลาทำงานจริง (มั้ง)
+startSocket.join()
+startStream.join()
--- Raspberry Pi/Visual Studio/Doorbell/SocketServer.py
+++ Raspberry Pi/Visual Studio/Doorbell/SocketServer.py
@@ -1,5 +1,7 @@
-﻿import socket                                         
+import socket                                         
 import threading
+import os
+from wifi import Cell,Scheme
 
 class _ClientThread(threading.Thread):
 
@@ -36,7 +38,42 @@ class _ClientPassword(_ClientThread):
     def getPassword(self):
         return '1234';
 
-class SocketGet(threading.Thread):
+class _ClientManageWifi(_ClientThread):
+
+    def handler(self):
+        if receive == 'scan':
+            print 'Connection from {}'.format(str(self.addr))
+            print 'Status: {}'.format(receive)
+            file = open('/etc/wpa_supplicant/wpa_supplicant.conf', 'r')
+            currentWifi = file.readlines()
+            dataSplit = currentWifi[4].split('ssid="')[1]
+            currentSSID = dataSplit.split('"')[0]
+            file.close()
+            data = Cell.all('wlan0')
+            self.clientsocket.send(str(len(data)) + '\r\n')
+            self.clientsocket.send(currentSSID + '\r\n')
+            for i in range(len(data)):
+                splitSSid = str(data[i]).split('=')[1]
+                self.clientsocket.send(str(splitSSid.split(')')[0]) + '\r\n')
+            print 'Exit ScanWifi'
+            
+        if 'modify' in receive:
+            print 'Status: {}'.format(receive)
+            ssid = receive.split(':')[1]
+            password = receive.split(':')[2]
+            editFile = open('/etc/wpa_supplicant/wpa_supplicant.conf','w')
+            editFile.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
+            editFile.write('update_config=1\n')
+            editFile.write('\nnetwork={\n     ssid="')
+            editFile.write(ssid)
+            editFile.write('"\n     psk="')
+            editFile.write(password)
+            editFile.write('"\n}')
+            editFile.close()
+            print 'Exit Modify Wifi'
+            os.system("sudo reboot")
+
+class SocketServer(threading.Thread):
 
     def run(self):
         host = socket.gethostname()                           
@@ -58,6 +95,8 @@ class SocketGet(threading.Thread):
                 newThread = _ClientPing(clientsocket, addr, threads)
             elif type == 'Password':
                 newThread = _ClientPassword(clientsocket, addr, threads)
+            elif type == 'ManageWifi':
+                newThread = _ClientManageWifi(clientsocket, addr, threads)
 
             if newThread != None:
                 newThread.start()
@@ -67,7 +106,3 @@ class SocketGet(threading.Thread):
             t.join()
 
         socketserver.close()
-
-start = SocketGet()
-start.start()
-start.join()
